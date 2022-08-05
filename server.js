9b3b4c300e3a735c3cf6ef5c5d6ede489d311c38
deep9888 <gundeep.singh@ikarus3d.com>
2022-08-06 00:49:21 +0530

    files route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const AWS = require('aws-sdk');
 const cors = require('cors');
-const PORT = 3032;
+const PORT = 3031;
 
 app.use(cors());
 app.use(express.json());
@@ -21,30 +21,50 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const email = req.body.email;
-    const password 
-    = req.body.password;
+    const password
+        = req.body.password;
     const today = new Date();
     const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const loginUsersDetails = {
-        TableName: 'Users',
-        Item: {
-            email: email,
-            password: password,
-            Date: date,
-            time: time
-        },
-    };
-    console.log('loginUsersDetails =      ==', loginUsersDetails);
-    docClient.put(loginUsersDetails, function (err, data) {
-        if (err) console.log('e==========', err)
-        else console.log('d============', data)
-    });
-    res.send('logged in');
+
+    // get dbEmail and dbPassword from aws
+
+    // compare above data with db's email and pswrd
+    if(email === dbEmail && password === dbPassword){
+        res.json({
+            msg: 'loggedin successfully'
+        })
+    }
 })
-app.post('/signup', (req,res) => {
+
+app.get('/files/:v', async (req, res)=>{
+    const v = req.params.v;
+console.log('v: ', v);
+    const params = {
+        KeyConditionExpression: "id = :s",
+        ExpressionAttributeValues: {
+            ":s": v
+        },
+        ProjectionExpression: "id, #URL, annotation",
+        ExpressionAttributeNames: { "#URL": "URL" },
+        TableName: "testing"
+    }
+    console.log(params);
+    let data;
+    docClient.query(params, function (err, fetchedData) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+           data = fetchedData
+        }
+    })
+    console.log(data);
+    res.json(data)
+})
+
+app.post('/signup', async (req, res) => {
     const fname = req.body.firstName;
     const lname = req.body.lastName;
     const emailSignUp = req.body.emailSignup;
@@ -57,8 +77,8 @@ app.post('/signup', (req,res) => {
         Item: {
             email: emailSignUp,
             password: passwordSignUp,
-            fname:fname,
-            lname:lname,
+            fname: fname,
+            lname: lname,
             Date: date,
             time: time
         },
