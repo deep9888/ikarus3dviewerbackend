21c185f8405f8623249e6a4e982f58483687d528
deep9888 <103727216+deep9888@users.noreply.github.com>
2022-09-05 19:21:07 +0530

    working on AWS s3

---
+++ aws-config.ts
@@ -0,0 +1,6 @@
+module.exports = {
+    awsCreds: {
+        accessKey: 'AKIAQC4W4IVSLTMT2ODW',
+        secretKey: '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'
+    }
+}
--- server.js
+++ server.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const AWS = require('aws-sdk');
+const formidable = require('formidable');
 const cors = require('cors');
-const PORT = 3031;
 
+
+const multer = require('multer');
+const storage = multer.memoryStorage() ;
+let upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        //if (mimeTypes.includes(file.mimetype)) {
+          return cb(null, true);
+        //}
+        //cb('File type not allowed', false);
+    }
+}).any();
+
+
+
+const PORT = 3031;
+const {
+    v4: uuidv4
+} = require('uuid');
 app.use(cors());
 app.use(express.json());
 
@@ -13,6 +32,13 @@ AWS.config.update({
     accessKeyId: process.env.accessKeyId,
     secretAccessKey: process.env.secretAccessKey,
 });
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey,
+    region: process.env.REGION
+});
+
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 app.use(function (req, res, next) {
@@ -23,32 +49,79 @@ app.use(function (req, res, next) {
 });
 app.post('/login', async (req, res) => {
     const email = req.body.email;
-    const password
-        = req.body.password;
-    const today = new Date();
-    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-    // get dbEmail and dbPassword from aws
-
-    // compare above data with db's email and pswrd
-    if(email === dbEmail && password === dbPassword){
-        res.json({
-            msg: 'loggedin successfully'
-        })
+    const password = req.body.password;
+    console.log(email)
+    const params = {
+        Key: {
+            "email": email
+        },
+        TableName: "signUpUsers"
     }
+    docClient.get(params, function (err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            if (Object.keys(data).length === 0) {
+                console.log("Empty")
+            } else {
+                dbEmail = data.Item.email;
+                dbPassword = data.Item.password;
+                if (email === dbEmail && password === dbPassword) {
+                    console.log("matching");
+                    res.json({
+                        msg: 'loggedin successfully',
+                        email,
+                        password
+                    })
+                } else {
+                    console.log("Not matching");
+                    res.json({
+                        msg: 'Email & Password not matching'
+                    })
+                }
+            }
+            // console.log(data);
+        }
+    });
+    // console.log(params);
+    // docClient.query(params, function (err, fetchedData) {
+    //     var dbEmail, dbPassword;
+    //     if (err) {
+    //         console.log("not matched");
+    //         //console.log("Error", err);
+    //     } else {
+    //         dbEmail = fetchedData.Items[0].email;
+    //         dbPassword = fetchedData.Items[0].password;
+    //     }
+    //     // compare above data with db's email and pswrd
+    //     console.log(email)
+    //     console.log(dbEmail)
+    //     if (email === dbEmail && password === dbPassword) {
+    //         console.log("matching");
+    //         res.json({
+    //             msg: 'loggedin successfully'
+    //         })
+    //     }else{
+    //         console.log("Not matching");
+    //         res.json({
+    //             msg: 'Email & Password not matching'
+    //         })
+    //     }
+    // })
 })
 
-app.get('/files/:v', async (req, res)=>{
+app.get('/files/:v', async (req, res) => {
     const v = req.params.v;
-console.log('v: ', v);
+    console.log('v: ', v);
     const params = {
         KeyConditionExpression: "id = :s",
         ExpressionAttributeValues: {
             ":s": v
         },
         ProjectionExpression: "id, #URL, annotation",
-        ExpressionAttributeNames: { "#URL": "URL" },
+        ExpressionAttributeNames: {
+            "#URL": "URL"
+        },
         TableName: "testing"
     }
     console.log(params);
@@ -57,14 +130,14 @@ console.log('v: ', v);
         if (err) {
             console.log("Error", err);
         } else {
-           data = fetchedData
+            data = fetchedData
         }
     })
     console.log(data);
     res.json(data)
 })
 
-app.post('/signup', async (req, res) => {
+app.post('/signup', express.json(), (req, res) => {
     const fname = req.body.firstName;
     const lname = req.body.lastName;
     const emailSignUp = req.body.emailSignup;
@@ -84,11 +157,98 @@ app.post('/signup', async (req, res) => {
         },
     };
     docClient.put(signUpUserDetails, function (err, data) {
+        if (err && err.code === "ConditionalCheckFailedException") {
+            console.log("Wrong Email or password")
+            return res.status(401).json({
+                msg: "Not account with this email."
+            });
+        } else if (err) res.json({
+            msg: "Insert Failed with some other reason"
+        });
+        else res.json({
+            msg: "Sucessfully inserted"
+        });
+    });
+})
+app.post('/saveDB', express.json(), (req, res) => {
+    console.log('click');
+    const id = uuidv4();
+    const annotation = req.body.annotations;
+    const url = req.body.URL;
+    //uniqueID.push(id);
+
+    var params3 = {
+        TableName: 'testing',
+        Item: {
+            id: id,
+            annotation: annotation,
+            URL: url
+        },
+    };
+    docClient.put(params3, function (err, data) {
         if (err) console.log('e==========', err)
         else console.log('d============', data)
     });
-    res.send('Signed Up Successfully');
+    res.send('Saved Successfully');
 })
+app.post('/saveGLB', (req, res) => {
+
+    // const form = formidable({
+    //     multiples: true
+    // });
+    // form.parse(req, (err, fields, files) => {
+    //     // console.log('fields: ', fields);
+    //     console.log(files['sendFile']);
+
+    //     // const params = {
+    //     //     Body: files.sendFile,
+    //     //     Bucket: process.env.BUCKET_NAME,
+    //     //     Key: fields.sendFileName
+    //     // }
+    //     // console.log(files.sendFile);
+    //     // const uploadData = s3.upload(params).promise();
+    //     // uploadData.then(function (result) {
+    //     //     console.log('RESULT ========== ', result.Location);
+    //     //     preSignedURL = result.Location;
+    //     // })
+
+    //     res.send({
+    //         success: true
+    //     });
+
+
+    // });
+
+    upload(req, res, (err) => {
+        if (err) {
+            // An error occurred when uploading to server memory
+            return res.status(502).json(err) ;
+        }
+        console.log(req.body.sendFileName);
+        console.log(req.files[0])
+
+        
+        
+        const params = {
+            Body: req.files[0],
+            Bucket: process.env.BUCKET_NAME,
+            Key: req.body.sendFileName
+        }
+        
+        const uploadData = s3.upload(params).promise();
+        uploadData.then(function (result) {
+            console.log('RESULT ========== ', result.Location);
+            preSignedURL = result.Location;
+        })
+
+            res.send({
+            success: true
+        });
+    })
+    
+})
+
+
 app.listen(PORT, () => {
     console.log('Server Running ', PORT);
 })
